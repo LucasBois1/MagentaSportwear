59ce9d7ebda87266e8547779a0465778ad62056d
lucasbois1 <lucasjkjk@gmail.com>
2021-05-20 22:58:17 -0300

    Add AdminPanel

---
+++ MagentaSportwear/Controllers/AdminPanelController.cs
@@ -0,0 +1,40 @@
+﻿using MagentaSportwear.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagentaSportwear.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+
+        private readonly ILogger<AdminPanelController> _logger;
+
+        public AdminPanelController(ILogger<AdminPanelController> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: AdminController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Articulos()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
