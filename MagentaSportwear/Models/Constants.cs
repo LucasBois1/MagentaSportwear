cfc6a858cd7f1f4c1bd36db63c6181c81c68012e
lucasbois1 <lucasjkjk@gmail.com>
2021-05-22 20:50:07 -0300

    Add AdminRole

---
+++ MagentaSportwear/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagentaSportwear.Models
+{
+    public class Constants
+    {
+        public const string AdministratorRole = "Administrator";
+    }
+}
