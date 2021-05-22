6c86b99b1de9e6957eee7de27a5ba5c6bd74cdf4
lucasbois1 <lucasjkjk@gmail.com>
2021-05-22 20:49:38 -0300

    Add RoleManagement

--- MagentaSportwear/Program.cs
+++ MagentaSportwear/Program.cs
@@ -14,7 +14,30 @@ namespace MagentaSportwear
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            InitializeDatabase(host);
+            host.Run();
         }
+
+        private static void InitializeDatabase(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.InitializeAsync(services).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services
+                        .GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "Error occurred seeding the DB.");
+                }
+            }
+        }
+
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
